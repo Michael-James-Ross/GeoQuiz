9719d763afd65751f669518033cf2ea701decb9c
= <av17lost@gmail.com>
2016-09-13 17:11:29 +0300

    Overrided onSaveInstanceState() for keep alive current index of question for app after configuration of app will change

--- app/src/main/java/com/onyshchenko/artem/geoquiz/QuizActivity.java
+++ app/src/main/java/com/onyshchenko/artem/geoquiz/QuizActivity.java
@@ -13,11 +13,12 @@ import model.Question;
 
 public class QuizActivity extends AppCompatActivity {
 
+    private static final String TAG = "QuizActivity";
+    private static final String CURRENT_INDEX_QUESTION_KEY = "CURRENT_INDEX_QUESTION_KEY";
     private Button trueBtn;
     private Button falseBtn;
     private ImageButton nextQuestionBtn;
     private ImageButton prevQuestionBtn;
-
     private TextView questionTextTxtView;
     private int currentQuestionIndex = 0;
     private Question [] questions = {new Question(R.string.question_capital, true),
@@ -28,14 +29,27 @@ public class QuizActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_quiz);
-        questionTextTxtView = (TextView) findViewById(R.id.questionTextTxtView);
+        gatherAllWidgets();
+
+        if(savedInstanceState != null) {
+            currentQuestionIndex = savedInstanceState.getInt(CURRENT_INDEX_QUESTION_KEY, 0);
+        }
+
         updateQuestion();
+        setEventListeners();
+    }
+
+    private void gatherAllWidgets() {
+        questionTextTxtView = (TextView) findViewById(R.id.questionTextTxtView);
         trueBtn = (Button)findViewById(R.id.trueBtn);
         falseBtn = (Button)findViewById(R.id.falseBtn);
         nextQuestionBtn = (ImageButton)findViewById(R.id.nextBtn);
         prevQuestionBtn = (ImageButton)findViewById(R.id.prevBtn);
+    }
 
+    private void setEventListeners() {
         trueBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -88,8 +102,12 @@ public class QuizActivity extends AppCompatActivity {
     }
 
     private void updateQuestion() {
-        int question = questions[currentQuestionIndex].getQuestionTextResId();
-        questionTextTxtView.setText(question);
+        try{
+            int question = questions[currentQuestionIndex].getQuestionTextResId();
+            questionTextTxtView.setText(question);
+        } catch (ArrayIndexOutOfBoundsException ex) {
+            Log.d(TAG, "Index was out of bounds", ex);
+        }
     }
 
     private void checkAnswer(boolean userPressed) {
@@ -101,5 +119,41 @@ public class QuizActivity extends AppCompatActivity {
             toastResId = R.string.incorrect_toast;
         }
         Toast.makeText(QuizActivity.this, toastResId, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "OnPause");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        Log.d(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(CURRENT_INDEX_QUESTION_KEY, currentQuestionIndex);
     }
 }
